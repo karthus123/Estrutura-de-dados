2c26eef8b2092527c33f0bbd0a3b4460ded1f11f
karthus123 <143654566+karthus123@users.noreply.github.com>
2023-11-26 15:56:03 -0300

    Create program.cs

---
+++ tabela hash/program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Hashtable tabelaHash = new Hashtable();
+
+        while (true)
+        {
+            Console.WriteLine("Digite um par de Chave e Valor (CPF NOME), ou 'sair' para encerrar:");
+            string entrada = Console.ReadLine();
+
+            if (entrada == null)
+                break;
+
+            string[] dados = entrada.Split(' ');
+            if (dados.Length == 2)
+            {
+                string cpf = dados[0];
+                string nome = dados[1];
+
+                if (!tabelaHash.ContainsKey(cpf))
+                {
+                    tabelaHash[cpf] = nome;
+                    Console.WriteLine("Dados inseridos com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("CPF já existe na tabela. Insira um CPF diferente.");
+                }
+            }
+            else if (entrada.ToLower() == "sair")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Formato de entrada inválido. Use 'CPF NOME'.");
+            }
+        }
+
+        Console.WriteLine("\nDados na tabela hash:");
+        foreach (DictionaryEntry entry in tabelaHash)
+        {
+            Console.WriteLine($"CPF: {entry.Key}, NOME: {entry.Value}");
+        }
+    }
+}
